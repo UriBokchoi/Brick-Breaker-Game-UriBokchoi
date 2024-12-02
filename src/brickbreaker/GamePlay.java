b694c1d872d7f449fd1c096a69a9aecffe8556c5
Oriol Cárdenas <47286375+UriBokchoi@users.noreply.github.com>
2024-12-02 22:32:20 +0100

    Add files via upload

--- src/brickbreaker/GamePlay.java
+++ src/brickbreaker/GamePlay.java
@@ -4,10 +4,26 @@
  */
 package brickbreaker;
 
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 /**
  *
  * @author User
  */
-public class GamePlay {
+public class GamePlay extends JPanel implements KeyListener,ActionListener{
+    
+    private  boolean play = false;
+    private int score=0, totalBrick = 21;
+    private Timer timer;
+    private int delay= 8;
+    
+    
+    
+    
+    
+    
     
 }
